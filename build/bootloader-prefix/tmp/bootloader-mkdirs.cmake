73ebde245d7aef1468b91bf239c1d948a1fe91d7
David Moreno <elarreglador@protonmail.com>
2025-04-04 12:51:05 +0200

    Primera compilacion funcional

--- build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -5,18 +5,18 @@ cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
   "/home/david/esp/esp-idf/components/bootloader/subproject"
-  "/home/david/esp/ESP32-s3_matter/build/bootloader"
-  "/home/david/esp/ESP32-s3_matter/build/bootloader-prefix"
-  "/home/david/esp/ESP32-s3_matter/build/bootloader-prefix/tmp"
-  "/home/david/esp/ESP32-s3_matter/build/bootloader-prefix/src/bootloader-stamp"
-  "/home/david/esp/ESP32-s3_matter/build/bootloader-prefix/src"
-  "/home/david/esp/ESP32-s3_matter/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/david/esp/panel_matter/build/bootloader"
+  "/home/david/esp/panel_matter/build/bootloader-prefix"
+  "/home/david/esp/panel_matter/build/bootloader-prefix/tmp"
+  "/home/david/esp/panel_matter/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/david/esp/panel_matter/build/bootloader-prefix/src"
+  "/home/david/esp/panel_matter/build/bootloader-prefix/src/bootloader-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "/home/david/esp/ESP32-s3_matter/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+    file(MAKE_DIRECTORY "/home/david/esp/panel_matter/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "/home/david/esp/ESP32-s3_matter/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "/home/david/esp/panel_matter/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
 endif()
